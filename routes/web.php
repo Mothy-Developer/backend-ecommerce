1a169ae9ce769d8a26535a82100c5553c278247e
ervalsa-san <ervalsananda@gmail>
2023-03-21 06:21:10 +0800

    Refactor: Rename and Foldering structure file

--- routes/web.php
+++ routes/web.php
@@ -28,6 +28,7 @@ Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+
 Route::get('/user', function () {
     return Inertia::render('User');
 })->middleware(['auth', 'verified'])->name('user');
